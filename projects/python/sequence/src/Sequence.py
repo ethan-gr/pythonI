6174ab7b69161d7348b7341de730f0d1aab38537
ethan-gr <ethangr@lcg.unam.mx>
2023-05-09 21:03:52 -0600

    function implemented
    Now it works as a function

--- projects/python/sequence/src/Sequence.py
+++ projects/python/sequence/src/Sequence.py
@@ -10,6 +10,7 @@ AUTHOR
 
 DESCRIPTION
 	Tells the start codon poosition in a dna secuence and shows the transcript
+	in a sequence given by the user
 
 CATEGORY
 	
@@ -17,12 +18,38 @@ USAGE
 	% py To_fasta_file.py
 
 '''
-# Definitions
-dna = 'AAGGTACGTCGCGCGTTATTAGCCTAAT'
 
-start = dna.find('TAC') # Start codon
-stop = dna.find('ATT')  # Stop codon
+def transcrit(sequence, startCodon='TAC', stopCodon='ATT', retrn='transcrit'):
+	'''
+	This function works with a sequence looking for the transcript by 
+	checking for the star and stop codon, it can return the transcrit 
+	or the start of the transcrit
+
+	Args:
+		- sequence: Is the string that contains the sequence
+		- startCodon: It defines the codon identified as start
+		- stoptCodon: It defines the codon identified as stop
+		- retrn: It can be two options ´tarnscrit´ or ´start´ and it
+				 defines what to return
+	Retuns:
+		- ´sequence[start:stop]´: The transcrit
+		- start: The position in which the transcript start
+	'''
+	# The search
+	start = sequence.find(startCodon)
+	stop = sequence.find(stopCodon)
+	if start == None or stop == None:
+		raise ValueError('It is not posible to find the tarnscript due to codons ausense')
+
+	# Returns
+	if retrn == 'transcrit': return sequence[start:stop]
+	elif retrn == 'start': return start
+	else: raise ValueError('The parameter retrn has is not in an acceptable parameter')
+
 
 # Prints
-print(f'TAC se encuentra en la posicion {start}')
-print(f'El transcrito es: {dna[start:stop]}')
+print('Ingresa una secuencia a la cual buscar el punto d inicio del transcrito tanto como el transcrito')
+dna = input().upper()
+
+print(f"TAC se encuentra en la posicion {transcrit(dna, retrn='start')}\n\
+      El transcrito es: {transcrit(dna)}")
